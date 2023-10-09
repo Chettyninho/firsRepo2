f339870326f315fe37263ddd11fb820718607309
miguel <miguelacheto@gmail.com>
2023-10-09 09:31:31 +0200

    Comit tras modificacion

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 public static void main(String[] args){
-System.out.println("Hola mundo");
+System.out.println("a este paso me meto un tiro en las pelotas");
+System.out.println("te rieh, he ido con 100 y pico de vena en alhol en sangre y de nunca man parao");
 }
 }
