cd22111afa0ac1ca31055d8fded256d0c8994b1e
miguel <miguelacheto@gmail.com>
2023-10-09 10:19:04 +0200

    conSumatorioAñadido

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class HelloWorld {
 public static void main(String[] args){
 System.out.println("a este paso me meto un tiro en las pelotas");
 System.out.println("te rieh, he ido con 100 y pico de vena en alhol en sangre y de nunca man parao");
+int x = 0;
+for(int i =0; i<5;i++){
+x += 1;
+System.out.println(x);
+}
 }
 }
