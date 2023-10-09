57b93143cce28cba7658fac504be38b272c6a09c
miguel <miguelacheto@gmail.com>
2023-10-09 10:27:05 +0200

    addFunction

--- Main.java
+++ Main.java
@@ -2,10 +2,13 @@ public class HelloWorld {
 public static void main(String[] args){
 System.out.println("a este paso me meto un tiro en las pelotas");
 System.out.println("te rieh, he ido con 100 y pico de vena en alhol en sangre y de nunca man parao");
-int x = 0;
-for(int i =0; i<5;i++){
-x += 1;
-System.out.println(x);
+int x = 10;
+int y  = 1;
+
+Atarwm.out.println(addNumbers(x,y));
 }
+
+public static int addNumbers(int x, int y){
+return (x+y);
 }
 }
